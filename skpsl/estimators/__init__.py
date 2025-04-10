5f8d307c8af4e54e68d3da14efe88873a3f85d81
Seungwoo Lee <lees@cip.ifi.lmu.de>
2025-04-10 21:54:06 +0200

    added ga
    Signed-off-by: Seungwoo Lee <lees@cip.ifi.lmu.de>

--- skpsl/estimators/__init__.py
+++ skpsl/estimators/__init__.py
@@ -1,4 +1,5 @@
 from .probabilistic_scoring_list import ProbabilisticScoringList
 from .probabilistic_scoring_system import ProbabilisticScoringSystem
+from .ga_probabilistic_scoring_list import GeneticProbabilisticScoringList
 
-__all__ = ["ProbabilisticScoringSystem", "ProbabilisticScoringList"]
+__all__ = ["ProbabilisticScoringSystem", "ProbabilisticScoringList", "GeneticProbabilisticScoringList"]
